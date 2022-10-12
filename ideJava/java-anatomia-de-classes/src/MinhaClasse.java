5808d7496e58071236d42b289f25223dbfcc3ff6
Lucas Cadilhe <lucas_cadilhe@hotmail.com>
2022-10-12 12:01:46 -0300

    estudo java iniciante

--- ideJava/java-anatomia-de-classes/src/MinhaClasse.java
+++ ideJava/java-anatomia-de-classes/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
     public static void main (String[] args) {
+        String primeiroNome = "Lucas";
+        String segundoNome = "Mariana";
 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Qual o melhor casal que existe? " + primeiroNome.concat(" e ").concat(segundoNome);
     }
 }
